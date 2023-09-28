e4d6d8e03f0ac6fbf286d8e52ba6ab39ecbf7e3c
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-09-28 12:16:26 +0530

    second commit

--- src/books.js
+++ src/books.js
@@ -1,3 +1,27 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
 
 
+export const books=[
+{
+    author:'javid',
+    title:'interesting facts  for curios minds ',
+    img:img1,
+    id:1,
+},
+{
+    author:'anshid',
+    title:'atomic habits ',
+    img:img2,
+    id:2,
 
+},
+{
+    author:'ParveshKhan',
+    title:'Fairy Tale ',
+    img:img3,
+    id:3,
+
+}
+]
